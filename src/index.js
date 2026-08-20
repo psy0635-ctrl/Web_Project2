c17063cd0bf9487364c75fad62b5c6f573571357
psy0635-ctrl <psy0635@gmail.com>
2026-08-20 11:07:54 +0900

    TodoList_0820

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import TodoList from "./01/TodoList";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>
 );
 
