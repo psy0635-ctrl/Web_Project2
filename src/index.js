dbda74079dfe624e23131d3297e9525f80cfa33c
psy0635-ctrl <psy0635@gmail.com>
2026-08-27 11:50:07 +0900

    React_0827

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from "./03/Library";
+import './03/Book.css';
 
-import TodoList from "./01/TodoList";
+// import TodoList from "./01/TodoList";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoList />
+    <Library/>
   </React.StrictMode>
 );
 
